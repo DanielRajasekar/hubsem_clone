9133da6d634ba8f9d4e4a8696b2816aeb37bdc8f
DanielRajasekar <105115346+DanielRajasekar@users.noreply.github.com>
2024-07-06 18:58:01 +0530

    Add files via upload

---
+++ hubsem clone/script.js
@@ -0,0 +1,36 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    speed:1000,
+    
+    autoplay :{
+      delay:2000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false
+    },
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
